bc5366e4c18c1928f8ae50628a6fbbfc3a187212
Bright <brightiniabasi@gmail.com>
2022-06-03 08:08:07 +0100

    Task completed :horse:

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-import sys
+from sys import exit, argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    argc = len(sys.argv)
-    arg = sys.argv
+    argc = len(argv)
     if argc < 2:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    else:
-        a = int(arg[1])
-        b = int(arg[3])
-        if arg[2] == '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-            sys.exit(0)
-        if arg[2] == '-':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-            sys.exit(0)
-        if arg[2] == '*':
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-            sys.exit(0)
-        if arg[2] == '/':
-            print("{} / {} = {}".format(a, b, div(a, b)))
-            sys.exit(0)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    operators = ['+', '-', '*', '/']
+    functions = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+
+    # when user's input(intended operator) does
+    # not match any item in operators tuple
+    if not (argv[2] in operators):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    # when user's input matches item in operator tuple
+    # the corresponding function is executed
+    for op, func in zip(operators, functions):
+        if op == argv[2]:
+            break
+    print(f"{a} {argv[2]} {b} = {func}")
+    exit(0)
