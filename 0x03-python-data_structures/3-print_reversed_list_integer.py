3a172c0d68058c7e837bac6ce4700016360ab5da
Bright <brightiniabasi@gmail.com>
2022-06-05 02:27:40 +0100

    Task completed :penguin:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
     def print_reversed_list_integer(my_list=[]):
-        my_list.reverse()
-        
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        # to access individual elements in reversed order
+        for i in reversed(my_list):
+            print("{:d}".format(i))
