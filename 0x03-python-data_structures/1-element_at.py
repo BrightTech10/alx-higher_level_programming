1db6a350b64c2618092feedb1ad107eb061ff22b
Bright <brightiniabasi@gmail.com>
2022-06-05 01:45:22 +0100

    Improved performance :horse:

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,10 @@
 if __name__ != "__main__":
     def element_at(my_list, idx):
         size = len(my_list)
-        if idx < 0 or idx > size:
-            return None
-        else:
+        if idx >= 0 and idx < size:
             for i in range(size):
                 if i == idx:
                     break
-        return my_list[i]
+            return my_list[i]
+        else:
+            return None
