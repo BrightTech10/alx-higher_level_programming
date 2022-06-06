050c8e511830895ff03d72af5059a0777455b058
Bright <brightiniabasi@gmail.com>
2022-06-06 04:34:53 +0100

    Bug fixed :bug:

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,13 @@
 
 if __name__ != "__main__":
     def no_c(my_string):
+        list_copy = []
+        list_copy[:] = my_string
+        string_copy = ""
 
-        for char in my_string:
-            new_string = my_string.replace('c', "").replace('C', "")
+        for i in range(len(list_copy)):
+            if list_copy[i] == 'c' or list_copy[i] == 'C':
+                list_copy[i] = ''
+            new_string = "".join(map(str, list_copy))
+
         return new_string
