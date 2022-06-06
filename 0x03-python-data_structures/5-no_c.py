354bfba6f8fdf5768f2bea15585f97563bd119a2
Bright <brightiniabasi@gmail.com>
2022-06-06 10:00:38 +0100

    Code changes :penguin:

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,6 +9,6 @@ if __name__ != "__main__":
         for i in range(len(list_copy)):
             if list_copy[i] == 'c' or list_copy[i] == 'C':
                 list_copy[i] = ''
-            new_string = "".join(map(str, list_copy))
+            new_string = "".join(list_copy)
 
         return new_string
