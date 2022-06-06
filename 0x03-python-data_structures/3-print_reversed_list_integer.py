4f5e71098e3c7b045d4e71e80cccf72a2472383c
Bright <brightiniabasi@gmail.com>
2022-06-06 10:26:11 +0100

    Bug fix :bug:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+from audioop import reverse
+
+
 if __name__ != "__main__":
     def print_reversed_list_integer(my_list=[]):
-        # to access individual elements in reversed order
-        for i in reversed(my_list):
-            print("{:d}".format(i))
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
