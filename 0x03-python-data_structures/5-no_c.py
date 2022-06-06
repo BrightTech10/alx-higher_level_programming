7a688a0a349ad65e459192460c6ba00a7f878fa6
Bright <brightiniabasi@gmail.com>
2022-06-06 19:22:23 +0100

    Working :construction:

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,12 +3,13 @@
 if __name__ != "__main__":
     def no_c(my_string):
         list_copy = []
-        list_copy[:] = my_string
-        string_copy = ""
+        new_string = ""
 
-        for i in range(len(list_copy)):
-            if list_copy[i] == 'c' or list_copy[i] == 'C':
-                list_copy[i] = ''
-            new_string = "".join(list_copy)
+        if isinstance(my_string, str):
+            list_copy[:] = my_string
+            for i in range(len(list_copy)):
+                if list_copy[i] == 'c' or list_copy[i] == 'C':
+                    list_copy[i] = ''
+                new_string = "".join(list_copy)
 
         return new_string
