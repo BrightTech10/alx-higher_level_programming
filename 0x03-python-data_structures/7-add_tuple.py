28d151dec09fb8dac6d28ef671a3918e496c3b6b
Bright <brightiniabasi@gmail.com>
2022-06-07 17:29:40 +0100

    Updated :horse:

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,14 +15,14 @@ if __name__ != "__main__":
         new_tuple = ()
         value1 = (0, )
         value2 = (0, 0)
+        if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
+            if len(tuple_a) == 1 or len(tuple_b) == 1:
+                tuple_a = tuple_a + value1
+                tuple_b = tuple_b + value1
 
-        if len(tuple_a) == 1 or len(tuple_b) == 1:
-            tuple_a = tuple_a + value1
-            tuple_b = tuple_b + value1
+            elif len(tuple_a) == 0 or len(tuple_b) == 0:
+                tuple_a = tuple_a + value2
+                tuple_b = tuple_b + value2
 
-        elif len(tuple_a) == 0 or len(tuple_b) == 0:
-            tuple_a = tuple_a + value2
-            tuple_b = tuple_b + value2
-
-        new_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
-        return new_tuple
+            new_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
+            return new_tuple
