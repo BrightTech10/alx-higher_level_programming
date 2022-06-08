bad80d9af78a8098b2fc2a153889e3ad5386113a
Bright <brightiniabasi@gmail.com>
2022-06-08 22:15:41 +0100

    Code changes :art:

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -12,8 +12,4 @@ def uniq_add(my_list=[]):
     Return:
         returns sum of unique integers
     """
-    new_list = list(set(my_list))  # This retrieves all unique integers
-    sum = 0
-    for i in range(len(new_list)):
-        sum += new_list[i]
-    return sum
+    return sum(set(my_list))
