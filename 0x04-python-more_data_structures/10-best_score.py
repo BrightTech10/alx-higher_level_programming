24da3393494098cb80d284d3e2dca6e7e179f5b1
Bright <brightiniabasi@gmail.com>
2022-06-11 04:36:29 +0100

    Updated :penguin:

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-
-import re
-
-
 def best_score(a_dictionary):
+    """ A function that computes the maximum value in a dictionary
+
+    Args:
+        a_dictionary: a dictionary
+
+    Return:
+        returns the key with the biggest integer value
+    """
     if a_dictionary:
         biggest = max(a_dictionary, key=lambda key: a_dictionary[key])
         return biggest
