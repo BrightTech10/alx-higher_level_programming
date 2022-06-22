c96e3e8c669211fe16ef283ed655275345fff387
Bright <brightiniabasi@gmail.com>
2022-06-22 11:34:31 +0100

    Task 6 done

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+""" Defines a square """
+
+
+class Square:
+    """ Creates square class """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initializes object
+
+        Args:
+            size: size of object
+
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ Returns object size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets object size
+
+        Args:
+            value: size to be set
+
+        Raises:
+            if size is not an integer, TypeError exception is raised
+            if size is less than 0, ValuError exception is raised
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Returns position """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Sets object's position
+
+        Args:
+            value: position to be set
+
+        Raises:
+            if value is not a tuple of two integers,
+            TypeError exception is raised
+
+        """
+        if not isinstance(value, tuple) or len(value) > 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ Computes the square area
+
+        Return:
+            returns the current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ prints square with the character '#' in stdout """
+        if self.__size == 0:
+            print()
+        if self.__position[1] > 0:
+            print()
+        for i in range(self.__size):
+            print("".rjust(self.__position[0]), end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
