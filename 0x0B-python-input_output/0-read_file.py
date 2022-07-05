cfc811b6df113308122ba092f2359c736066b063
Bright <brightiniabasi@gmail.com>
2022-07-05 14:55:50 +0100

    Removed new-line :horse:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     """
     with open(file=filename, mode='r', encoding='utf-8') as file:
         read = file.read()
-        print(read)
+        print(read, end='')
