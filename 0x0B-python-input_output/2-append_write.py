56e228041aa26ae725f4281ef5131379e05e6994
Bright <brightiniabasi@gmail.com>
2022-07-05 14:38:23 +0100

    Bug fixes :bug:

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,15 +7,12 @@ def append_write(filename="", text=""):
     Appends a string at the end of a text file
 
     Args:
-        filename: file to append string
+        filename: path to file
         text: string to be appended
 
     Returns:
         returns the number of characters appended
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(text)
-
     with open(filename, mode='a', encoding='utf-8') as file_a:
         a_file = file_a.write(text)
         return a_file
