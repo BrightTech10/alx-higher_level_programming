df51814d4b8bdd2632ae07f2a23543e3cc38811d
Bright <brightiniabasi@gmail.com>
2022-07-06 01:29:38 +0100

    Handled exception :horse:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
 list_obj = []
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(list_obj, filename)
 
 arg_length = len(argv)
 
@@ -14,4 +18,4 @@ if arg_length > 1:
     for i in range(1, arg_length):
         list_obj.append(argv[i])
 
-save_to_json_file(list_obj, filename)
+    save_to_json_file(list_obj, filename)
