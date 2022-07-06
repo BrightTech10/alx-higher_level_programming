6662cdeeb85d42c8654ffc4982d7fc1e93f526fe
Bright <brightiniabasi@gmail.com>
2022-07-06 01:28:02 +0100

    Task 7 :construction:

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 7-add_item.py """
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+list_obj = []
+
+arg_length = len(argv)
+
+if arg_length > 1:
+    for i in range(1, arg_length):
+        list_obj.append(argv[i])
+
+save_to_json_file(list_obj, filename)
