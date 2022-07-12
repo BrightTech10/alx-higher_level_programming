2c7fc2692ae9342ea38a36115de6b0cb34a2e6b0
Bright <brightiniabasi@gmail.com>
2022-07-12 21:30:45 +0100

    Bug fixes :bug:

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,7 +28,7 @@ class Square(Rectangle):
             raise ValueError("x must be >= 0")
 
         if type(y) is not int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         elif y < 0:
             raise ValueError("y must be >= 0")
 
